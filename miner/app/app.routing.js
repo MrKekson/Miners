ee364cf914ae91ad7b742f7895882379b4188170
MrKekson <beer.olius@gmail.com>
2016-08-29 11:05:13 +0200

    cleanup4

---
+++ miner/app/app.routing.js
@@ -0,0 +1,17 @@
+// import { Routes, RouterModule } from '@angular/router';
+// const appRoutes: Routes = [
+//   { path: 'Mine', component: MineWord },
+//   {
+//     path: 'heroes',
+//     component: HeroListComponent,
+//     data: {
+//       title: 'Heroes List'
+//     }
+//   },
+//   { path: 'hero/:id', component: HeroDetailComponent },
+//   { path: '**', component: PageNotFoundComponent }
+// ];
+// export const appRoutingProviders: any[] = [
+// ];
+// export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes); 
+//# sourceMappingURL=app.routing.js.map
